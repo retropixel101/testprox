f565bea04af897b97183dd122d065d19df7a86de
retropixel101 <retropixel404@gmail.com>
2026-09-12 03:28:05 -0400

    fix

--- sw.js
+++ sw.js
@@ -7,19 +7,27 @@ if (navigator.userAgent.includes("Firefox")) {
 
 importScripts("/testprox/scram/scramjet.all.js");
 
-const { ScramjetServiceWorker } = $scramjetLoadWorker();
-const scramjet = new ScramjetServiceWorker();
+const { ScramjetServiceWorker } =
+    $scramjetLoadWorker();
+
+const scramjet =
+    new ScramjetServiceWorker();
 
 async function handleRequest(event) {
+
     await scramjet.loadConfig();
 
     if (scramjet.route(event)) {
-        return scramjet.fetch(event);
+        return await scramjet.fetch(event);
     }
 
     return fetch(event.request);
 }
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event));
+
+    event.respondWith(
+        handleRequest(event)
+    );
+
 });
