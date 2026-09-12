17cb41276cf7d2b235ed3e6aff13a2932feed2f0
retropixel101 <retropixel404@gmail.com>
2026-09-12 01:32:04 -0400

    fix

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ if (navigator.userAgent.includes("Firefox")) {
 // blocklist by s16 and swium - blocklist by s16 and swium - blocklist by s16 and swium - blocklist by s16 and swium - blocklist by s16 and swium
 // IMPORTANT: MAKE SURE TO REROUTE THIS TO YOUR REAL FILE
 // YOU MUST DO THIS OR SCRAMJET WILL NOT WORK
-importScripts("/sail/scram/scramjet.all.js");
+importScripts("/scram/scramjet.all.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
