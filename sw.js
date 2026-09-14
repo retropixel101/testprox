631e2370d2d0fbd55e0b32c2d0ac8085ba4690f0
retropixel101 <retropixel404@gmail.com>
2026-09-13 21:09:17 -0400

    fix

---
+++ sw.js
@@ -0,0 +1,17 @@
+// sw.js
+importScripts("/scramjet/scramjet.all.js");
+
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const scramjet = new ScramjetServiceWorker();
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      await scramjet.loadConfig();
+      if (scramjet.route(event)) {
+        return scramjet.fetch(event);
+      }
+      return fetch(event.request);
+    })()
+  );
+});
